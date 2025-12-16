60e9294ffb023c568c4d21e93d1df6d8982bdeb1
beauwilliams <beau.williams@icloud.com>
2025-12-16 15:52:58 +1100

    feat: log fac details

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { app } from "./app.js";
 import { PORT } from "./config.js";
 
 app.listen(PORT);
-console.log("Facilitator listening");
+console.log(`Facilitator listening on http://localhost:${PORT}`);
